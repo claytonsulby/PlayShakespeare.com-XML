e8faf51c0850a008fd37d47b77c8d84fc440350e
Clayton Sulby <claytonsulby@gmail.com>
2023-06-25 12:20:10 -0400

    swift package manager

---
+++ Sources/PlayShakespearePackage/PlayShakespearePackage.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Clayton Sulby on 6/25/23.
+//
+
+public struct PlayShakespearePackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
